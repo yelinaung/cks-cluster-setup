34a31fe3832046f66b1abefe72b95f4fe7a13b96
Ye Lin Aung <me@yelinaung.com>
2022-05-17 09:48:45 +0800

    fmt

--- cks-cluster/nodes/output.tf
+++ cks-cluster/nodes/output.tf
@@ -1,5 +1,5 @@
 output "master_node_ip" {
-  value       = "${google_compute_instance.cks-master-node.network_interface.0.access_config.0.nat_ip}"
+  value       = google_compute_instance.cks-master-node.network_interface.0.access_config.0.nat_ip
   description = "Master Node IP"
 }
 
